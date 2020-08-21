ab31e37b27792c99de133e23d0db93b3d5f42763
solomon <githirasolomon@gmail.com>
2020-08-21 21:03:10 +0300

    Updates

---
+++ precache-manifest.7ee6204ae38845ab403e0eb456c03190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb6b53b7a13a1e17dc43d1a6e23230a",
+    "url": "/github.io/todo_app-reactjs2/index.html"
+  },
+  {
+    "revision": "3a9ef0b8408354ecae38",
+    "url": "/github.io/todo_app-reactjs2/static/css/main.1c1b8031.chunk.css"
+  },
+  {
+    "revision": "6df3ef927d49e705cd88",
+    "url": "/github.io/todo_app-reactjs2/static/js/2.7484def5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/todo_app-reactjs2/static/js/2.7484def5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a9ef0b8408354ecae38",
+    "url": "/github.io/todo_app-reactjs2/static/js/main.983696b6.chunk.js"
+  },
+  {
+    "revision": "5ee4ec2faaf743aaf601",
+    "url": "/github.io/todo_app-reactjs2/static/js/runtime-main.de490d09.js"
+  }
+]);

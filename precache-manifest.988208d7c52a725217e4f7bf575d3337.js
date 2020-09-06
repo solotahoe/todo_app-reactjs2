0e4b67d70edd2962377c5cdf630e374f0b7670a7
solomon <githirasolomon@gmail.com>
2020-09-07 00:37:14 +0300

    Updates

---
+++ precache-manifest.988208d7c52a725217e4f7bf575d3337.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a531b339c7bf62aecd9d70c9d54d42c",
+    "url": "/todo_app-reactjs2/index.html"
+  },
+  {
+    "revision": "4bda5e869900166dd62c",
+    "url": "/todo_app-reactjs2/static/css/main.1c1b8031.chunk.css"
+  },
+  {
+    "revision": "9d37c963c212a2c9957f",
+    "url": "/todo_app-reactjs2/static/js/2.de57cd6a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo_app-reactjs2/static/js/2.de57cd6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bda5e869900166dd62c",
+    "url": "/todo_app-reactjs2/static/js/main.b0d417ff.chunk.js"
+  },
+  {
+    "revision": "3fea7cdd3836b2f2c5ca",
+    "url": "/todo_app-reactjs2/static/js/runtime-main.0f5bc8ab.js"
+  },
+  {
+    "revision": "0bb247cd868af2718977fd4d764c5872",
+    "url": "/todo_app-reactjs2/static/media/afro.0bb247cd.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0fa5db8d074ddfc10934679642cd7c5b.js"
+  "/todo_app-reactjs2/precache-manifest.988208d7c52a725217e4f7bf575d3337.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo_app-reactjs2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
